22b01c58a1cfb9b4316c56149031c1fe65892927
Maria <marycarol.00@gmail.com>
2024-08-26 10:49:17 -0300

    feat: quinto requisito

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, news
 
 urlpatterns = [
   path('', index, name='home-page'),
+  path('news/<int:id>/', news, name='news-details-page'),
 ]
--- news/views.py
+++ news/views.py
@@ -5,3 +5,8 @@ from news.models import News
 def index(request):
     context = {"news_list": News.objects.all()}
     return render(request, 'home.html', context)
+
+
+def news(request, id):
+    context = {"news_details": News.objects.get(id=id)}
+    return render(request, 'news_details.html', context)
